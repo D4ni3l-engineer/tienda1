68b46ec69d5e2f2ab7c043752824b20c771bfa16
Akiel <carhuasdaniel0@gmail.com>
2025-07-12 22:13:30 -0500

    Update TiendaApplication.java

--- src/main/java/com/web/tienda/TiendaApplication.java
+++ src/main/java/com/web/tienda/TiendaApplication.java
@@ -2,21 +2,12 @@ package com.web.tienda;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class TiendaApplication implements WebMvcConfigurer{
+public class TiendaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TiendaApplication.class, args);
-	}
-
-	@Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        // Permitir acceso a archivos de la carpeta uploads/
-        registry.addResourceHandler("/uploads/**")
-                .addResourceLocations("file:uploads/");
+    public static void main(String[] args) {
+        SpringApplication.run(TiendaApplication.class, args);
     }
-
 }
+
