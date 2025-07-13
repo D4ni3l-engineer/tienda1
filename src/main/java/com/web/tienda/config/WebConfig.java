40dea9725251d638fdc1a289d78e4532b76a0a9c
Akiel <carhuasdaniel0@gmail.com>
2025-07-12 22:15:13 -0500

    Update WebConfig.java

--- src/main/java/com/web/tienda/config/WebConfig.java
+++ src/main/java/com/web/tienda/config/WebConfig.java
@@ -1,15 +1,20 @@
 package com.web.tienda.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/producto/**")
-                .addResourceLocations("file:uploads/producto/");
+    public void addCorsMappings(CorsRegistry registry) {
+        // Permite CORS (Cross-Origin Resource Sharing) para todas las rutas
+        registry.addMapping("/**").allowedOrigins("http://localhost:3000"); // Cambia la URL por la de tu frontend
     }
+
+    // Puedes agregar más configuraciones, por ejemplo, recursos estáticos o interceptores
 }
