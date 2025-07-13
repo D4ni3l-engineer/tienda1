1b682b17bc00352879e606ae7fa14c4bc57beaec
Akiel <carhuasdaniel0@gmail.com>
2025-07-12 22:13:12 -0500

    Update TiendaApplicationTests.java

--- src/test/java/com/web/tienda/TiendaApplicationTests.java
+++ src/test/java/com/web/tienda/TiendaApplicationTests.java
@@ -4,10 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TiendaApplicationTests {
+public class TiendaApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    public void contextLoads() {
+        // El test simplemente verifica que el contexto de Spring se carga correctamente.
+    }
 }
+
